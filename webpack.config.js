e7cd4c57ad804fff289c3996c66ef427f1189076
Chris Loveless <chriskloveless@gmail.com>
2022-11-13 13:22:19 -0800

    add error message to user logic

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
-    contentBase: './dist'
+    // contentBase: './dist'
   },
   devtool: 'eval-source-map',
   plugins: [
